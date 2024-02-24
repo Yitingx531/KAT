5371c7aa609a01c66e85621a56c226db2880918b
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-24 16:08:54 -0500

    finish with the first round of styling
    Co-authored-by: Thomas Ortiz thomasortiz444@gmali.com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import MainContainer from '../components/MainContainer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavContainer from './NavContainer';
-import FeaturesPageContainer from './FeaturesPageContainer'
+import AboutPageContainer from './AboutPageContainer'
 
 export default function App(){
 
   return (
     <div>
     <Router>
-    <NavContainer/>
+      <NavContainer/>
       <Routes>
           <Route path="/" element={<MainContainer/>}></Route>
-          <Route path="/about" element={<FeaturesPageContainer/>}></Route>
+          <Route path="/about" element={<AboutPageContainer/>}></Route>
       </Routes>
     </Router>
     </div>
